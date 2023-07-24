6e581018dc0e2651d5909ccaaea0d95b7f11db34
Demian <demyan.korotchenko@gmail.com>
2023-07-24 14:46:43 +0200

    123

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Notiflix from 'notiflix';
+import Swiper from 'swiper';
+import 'swiper/css';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
+import './js/test';
+import './js/tasty-backend-api';
+import './js/mobile-menu';
+import './js/popular';
